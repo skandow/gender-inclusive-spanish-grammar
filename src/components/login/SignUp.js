2ceec687d4a85525ea25d8f3a3f28167d9b072f4
skandow <kando1sr@gmail.com>
2020-08-28 12:35:21 -0500

    login working

---
+++ src/components/login/SignUp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class SignUp extends Component {
+    
+    render() {
+        return (
+            <div className="signup">
+               Let's sign up
+            </div>
+        )
+    }
+}
+  
+export default SignUp
